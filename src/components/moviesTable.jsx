c6fb0e019e04bddc98314e5531eb3c1d6d5c847a
Prohor722 <prohor.b722@gmail.com>
2021-08-31 13:27:02 +0600

    sorting done!

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,35 +1,27 @@
 import React, { Component } from 'react'
 import Like from './common/like';
+import TableHeader from './common/tableheader'
 
 class moviesTable extends Component {
+    columns = [
+        { path: "title", label: "Title" },
+        { path: "genre.name", label: "Genre" },
+        { path: "numberInStock", label: "Stock" },
+        { path: "dailyRentalRate", label: "Rate" },
+        { key: "blank1" },
+        { key: "blank2" }
+    ];
 
-    raiseSort = path => {
-        const sortColmn = { ...this.props.sortColmn };
-
-        if (sortColmn.path === path) {
-            sortColmn.order = (sortColmn.order === "asc") ? console.log("desc") : console.log("asc");
-        }
-        else {
-            sortColmn.path = path;
-            sortColmn.order = "asc";
-        }
-        this.props.onSort(sortColmn);
-    }
     render() {
-        const { movies, onDelete, onLike } = this.props;
+        const { movies, onDelete, onLike, onSort, sortColumn } = this.props;
 
         return (
             <table className="table">
-                <tbody>
-                    <tr>
-                        <th onClick={() => this.raiseSort('title')}>Title</th>
-                        <th onClick={() => this.raiseSort('genre.name')}>Genre</th>
-                        <th onClick={() => this.raiseSort('numberInStock')}>Stock</th>
-                        <th onClick={() => this.raiseSort('dailyRentalRate')}>Rate</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                </tbody>
+                <TableHeader
+                    columns={this.columns}
+                    sortColumn={sortColumn}
+                    onSort={onSort}
+                />
                 <tbody>
                     {movies.map(movie =>
                         <tr key={movie._id}>
